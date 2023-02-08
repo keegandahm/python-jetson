7cecd71ef73dc27b6ebf3087dcf1ccd6de5d668f
Keegan Dahm <keegan@reverse.sh>
2023-02-08 10:43:11 -0800

    Added aarch64 support

--- flake.nix
+++ flake.nix
@@ -2,18 +2,22 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
 
   outputs = { self, nixpkgs }: let
-    pkgs = nixpkgs.legacyPackages.x86_64-linux;
-  in {
-    packages.x86_64-linux.python-jetson = pkgs.python310.pkgs.buildPythonPackage {
-      pname = "python-jetson";
-      version = "0.0.0";
-      src = ./.;
+    forAllSystems = f: nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" ] (system: f system);
+  in rec {
+    packages = forAllSystems (system: let pkgs = nixpkgs.legacyPackages.${system}; in {
+      python-jetson = pkgs.python310.pkgs.buildPythonPackage {
+        pname = "python-jetson";
+        version = "0.0.0";
+        src = ./.;
 
-      propagatedBuildInputs = [
-        pkgs.python3.pkgs.pyftdi
-      ];
+        propagatedBuildInputs = [
+          pkgs.python3.pkgs.pyftdi
+        ];
 
-      doCheck = false;
-    };
+        doCheck = false;
+      };
+    });
+
+    defaultPackage = forAllSystems (system: packages.${system}.python-jetson);
   };
 }
